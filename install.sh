02d537c9601637c70950cea735e76b3fdb3fcad9
duclm278 <palered548@gmail.com>
2023-11-12 17:26:51 +0700

    Add ansible-lint

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 read -s -p "Password: " tmp
 echo "$tmp" | sudo -S apt-get update
 echo "$tmp" | sudo -S apt-get install -y python3 python3-pip
-pip3 install ansible
+pip3 install ansible ansible-lint
 PATH=$HOME/.local/bin:$PATH
 ansible-playbook -i hosts -e ansible_become_password="$tmp" "$@" local.yml
